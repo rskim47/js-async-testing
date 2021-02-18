8a05267264d14d665957486faf807339dc56bb2b
rskim47 <45672828+rskim47@users.noreply.github.com>
2021-02-18 12:52:58 -0600

    completed initial testing

--- app.js
+++ app.js
@@ -1,19 +1,9 @@
-import { fetchData } from './http';
+import exports from 'webpack';
+
+const { printTitle } = require('./util');
 
 const button = document.querySelector('button');
 
-const loadTitle = () => {
-  return fetchData().then(extractedData => {
-    const title = extractedData.title;
-    const transformedTitle = title.toUpperCase();
-    return transformedTitle;
-  });
-};
-
-const printTitle = () => {
-  loadTitle().then(title => {
-    console.log(title);
-  });
-};
-
 button.addEventListener('click', printTitle);
+
+exports.printTitle = printTitle;
